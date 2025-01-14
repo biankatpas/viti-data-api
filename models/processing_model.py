3b99f3c2af87d5f3f72c93a55fd609f91de6707c
Bianka Passos <biankatpas@gmail.com>
2025-01-14 11:33:58 -0300

    adding docstring for processing model

--- models/processing_model.py
+++ models/processing_model.py
@@ -4,6 +4,19 @@ from .base import Base
 
 
 class Processing(Base):
+    """
+    Represents the processing data table.
+
+    Attributes:
+        id (int): Primary key for the table.
+        year (int): The year associated with the processing data.
+        variety (str): The variety of the processed product.
+        quantity (int, optional): The quantity of the processed product.
+
+    Constraints:
+        - UniqueConstraint: Ensures that the combination of 'year' and 'variety' is unique.
+    """
+    
     __tablename__ = "processing"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
