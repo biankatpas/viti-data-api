bf3e83a3304cca1e2be1128e01a00e4248f36d79
Bianka Passos <biankatpas@gmail.com>
2025-01-14 11:26:41 -0300

    adding docstring for scrape

--- api/routes/scrape.py
+++ api/routes/scrape.py
@@ -1,3 +1,17 @@
+"""
+This module handles scraping data from external sources and preparing it for storage or processing.
+
+Functions:
+    perform_scrape(year: int, page: str) -> pd.DataFrame:
+        Perform a scrape for a specific year and page, then translate the column names.
+
+    scrape_data(year: int, page: ScraperPages) -> pd.DataFrame:
+        Perform the actual scraping for a given year and page.
+
+    translate_columns(df: pd.DataFrame, mapping: dict) -> pd.DataFrame:
+        Translate column names in a DataFrame based on a dictionary mapping.
+"""
+
 import logging
 import pandas as pd
 
@@ -11,6 +25,20 @@ logger = logging.getLogger(__name__)
 
 
 def perform_scrape(year, page):
+    """
+    Perform a scrape for a specific year and page, then translate the column names.
+
+    Args:
+        year (int): The year for which data should be scraped.
+        page (str): The page to scrape, corresponding to one of the ScraperPages.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the scraped and translated data.
+
+    Raises:
+        ValueError: If the specified page is not valid.
+    """
+
     try:
         page_to_scrape = ScraperPages[page.upper()]
     except KeyError:
@@ -23,7 +51,20 @@ def perform_scrape(year, page):
 
 def scrape_data(year, page):
     """
-    Scrape data for the given year and page (ScraperPages).
+    Perform the actual scraping for a given year and page.
+
+    Args:
+        year (int): The year for which data should be scraped.
+        page (Enum): The page to scrape, represented as a ScraperPages enum value.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the scraped data.
+
+    Logs:
+        An error message if scraping fails.
+
+    Returns:
+        pd.DataFrame: The scraped data or None if an error occurs.
     """
 
     scraper = Scraper(year=year, page=page)
@@ -42,10 +83,24 @@ def translate_columns(df: pd.DataFrame, mapping: dict) -> pd.DataFrame:
 
     Args:
         df (pd.DataFrame): The DataFrame to translate.
-        mapping (dict): Dictionary mapping original column names to new names.
+        mapping (dict): A dictionary mapping original column names to new column names.
 
     Returns:
-        pd.DataFrame: DataFrame with translated column names.
+        pd.DataFrame: A DataFrame with translated column names.
+
+    Example:
+        Input DataFrame:
+            Produto  Quantidade
+            A        100
+            B        200
+
+        Mapping:
+            {"Produto": "product", "Quantidade": "quantity"}
+
+        Translated DataFrame:
+            product   quantity
+            A         100
+            B         200
     """
 
     filtered_mapping = {key: value for key, value in mapping.items() if key in df.columns}
