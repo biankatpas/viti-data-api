f0706886043b48b8831e94b35d30bcbc9eff36db
Bianka Passos <biankatpas@gmail.com>
2025-01-14 11:35:03 -0300

    adding docstring for production model

--- models/production_model.py
+++ models/production_model.py
@@ -4,6 +4,19 @@ from .base import Base
 
 
 class Production(Base):
+    """
+    Represents the production data table.
+
+    Attributes:
+        id (int): Primary key for the table.
+        year (int): The year associated with the production data.
+        product (str): The name of the product being produced.
+        quantity (int, optional): The quantity of the product produced.
+
+    Constraints:
+        - UniqueConstraint: Ensures that the combination of 'year' and 'product' is unique.
+    """
+    
     __tablename__ = "production"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
