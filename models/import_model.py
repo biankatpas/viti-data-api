e44dcd7f6b60db5df0b108396ce634d196c43b5e
Bianka Passos <biankatpas@gmail.com>
2025-01-14 11:32:43 -0300

    adding docstring for import model

--- models/import_model.py
+++ models/import_model.py
@@ -4,6 +4,20 @@ from .base import Base
 
 
 class Import(Base):
+    """
+    Represents the import data table.
+
+    Attributes:
+        id (int): Primary key for the table.
+        year (int): The year associated with the import data.
+        country (str): The origin country for the imported goods.
+        quantity (int, optional): The quantity of goods imported.
+        value (int, optional): The monetary value of the imported goods.
+
+    Constraints:
+        - UniqueConstraint: Ensures that the combination of 'year' and 'country' is unique.
+    """
+
     __tablename__ = "import"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
